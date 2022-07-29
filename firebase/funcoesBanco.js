9b2cefe1775f18380cab22de37880074285feae8
relatorioasplan <108020443+relatorioasplan@users.noreply.github.com>
2022-07-29 16:34:31 -0300

    Add files via upload

--- firebase/funcoesBanco.js
+++ firebase/funcoesBanco.js
@@ -5,24 +5,6 @@ function parser({ table }) {
 }
 
 function LerBD() {
-    //var municipio = document.getElementById("municipio").value;
-    // municipio = 'SÃO SIMÃO'
-    // var dbRef = firebase.database().ref();
-    // //  for (let i = 0; i < 581; i++){
-
-    // dbRef.child("/PlanilhaPresidencia").child(0).get().then((snapshot) => {
-    // if (snapshot.exists()) {
-    //     var teste = snapshot.val();
-
-    // // console.log(snapshot.val());
-    // console.log(teste);
-    // } else {
-    //     console.log("No data available");
-    // }
-    // }).catch((error) => {
-    //     console.error(error);
-    // });
-
     var url = 'https://opensheet.elk.sh/19m-nYHiUQW5Vvo38F21mGqkoLViLTv4etur92_zI4cU/Dados'; // ou https://opensheet.elk.sh/a09rksjodf[...]/Minhaaba
     fetch(url).then(response => response.json()).then((dados) => {
 
@@ -35,84 +17,81 @@ function LerBD() {
 
 function Processa(MeusDados) {
     var tamanhoTabela = MeusDados.length;
-    //console.log(tamanho); -> tamanho
-    //console.log(MeusDados[1]['MUNICIPIO']);
-
     var municipio_tela = document.getElementById("municipio").value;
-    var MUNICIPIO;
-    var PROCESSO;
-    var CONVENENTE;
-    var PROGRAMA;
-    var MODALIDADE;
-    var BAIRRO;
-    var OBJETO;
-    var DATA_DE_VIGENCIA;
-    var SITUACAO_DA_OBRA;
-    var QUANT_PREVISTA;
-    var ENCAMINHAMENTO_PARA_SOLUCAO;
-    var QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS;
-    var VALOR_TOTAL_DA_OBRA_PREVISTO_RS;
-    var RECURSO_FEDERAL_MUNICIPAL_OSC_PREVISTO_RS;
-    var RECURSO_ESTADUAL_PREVISTO_RS;
-    var RECURSO_ESTADUAL_ENTREGUE;
-    var IRREGULARIDADE;
-    var ACAO2;
-    var ACAO;
-    var controle = 0;
-    if (municipio_tela == "") {
 
+    if (municipio_tela == "") {
+        //Validação para nao ter municipio vazio na tela
     }
     else {
+        let MUNICIPIO;
+        let PROCESSO;
+        let CONVENENTE;
+        let PROGRAMA;
+        let MODALIDADE;
+        let BAIRRO;
+        let OBJETO;
+        let DATA_DE_VIGENCIA;
+        let SITUACAO_DA_OBRA;
+        let QUANT_PREVISTA;
+        let ENCAMINHAMENTO_PARA_SOLUCAO;
+        let QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS;
+        let VALOR_TOTAL_DA_OBRA_PREVISTO_RS;
+        let RECURSO_FEDERAL_MUNICIPAL_OSC_PREVISTO_RS;
+        let RECURSO_ESTADUAL_PREVISTO_RS;
+        let RECURSO_ESTADUAL_ENTREGUE;
+        let IRREGULARIDADE;
+        let ACAO2;
+        let controle = 0;
+
         document.getElementById("relatorio").value = ""; // limpa tela  
 
-        var teste = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
+        let teste = document.querySelector('input[name="inlineRadioOptions"]:checked').value;
         //Verificação se pega do ultimo relaotorio (1) - ou dados da planilha (0)
         if (teste === "0") {
             document.getElementById("relatorio").value += "*" + municipio_tela + "*\n";
             document.getElementById("relatorio").value += "\n_Crédito Parceria_\n";
             //FOR //IF
-            for (var i = 0; i < tamanhoTabela; i++) {//tamanhoTabela
+            for (let i = 0; i < tamanhoTabela; i++) {//tamanhoTabela
                 MUNICIPIO = MeusDados[i]['MUNICIPIO'];
                 if (MUNICIPIO == municipio_tela) {
                     PROGRAMA = MeusDados[i]['PROGRAMA'];
                     if (PROGRAMA != 'ALUGUEL SOCIAL' && PROGRAMA != 'GOIÁS SOCIAL') {
-                        PROCESSO = MeusDados[i]['PROCESSO'];
-                        CONVENENTE = MeusDados[i]['CONVENENTE'];
-
-                        MODALIDADE = MeusDados[i]['MODALIDADE'];
-                        BAIRRO = MeusDados[i]['BAIRRO'];
-                        OBJETO = MeusDados[i]['OBJETO'];
                         DATA_DE_VIGENCIA = MeusDados[i]['DATA DE VIGÊNCIA'];
-                        SITUACAO_DA_OBRA = MeusDados[i]['SITUACAO DA OBRA'];
-                        QUANT_PREVISTA = MeusDados[i]['QUANT. PREVISTA'];
-                        ENCAMINHAMENTO_PARA_SOLUCAO = MeusDados[i]['ENCAMINHAMENTO PARA SOLUÇÃO '];
-                        QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS = MeusDados[i]['QUANT. DE PARCELAS REALIZADAS / BENEFICIÁRIOS CADASTRADOS'];
-                        VALOR_TOTAL_DA_OBRA_PREVISTO_RS = MeusDados[i]['VALOR TOTAL DA OBRA - PREVISTO - R$'];
-                        RECURSO_FEDERAL_MUNICIPAL_OSC_PREVISTO_RS = MeusDados[i]['RECURSO FEDERAL/MUNICIPAL/OSC - PREVISTO - R$'];
-                        RECURSO_ESTADUAL_PREVISTO_RS = MeusDados[i]['RECURSO ESTADUAL - PREVISTO - R$'];
-                        RECURSO_ESTADUAL_ENTREGUE = MeusDados[i]['RECURSO ESTADUAL ENTREGUE - R$'];
-                        IRREGULARIDADE = MeusDados[i]['IRREGULARIDADE'];
-                        ACAO2 = MeusDados[i]['AÇÃO2'];
-                        ACAO = MeusDados[i]['AÇÃO'];
                         if (DATA_DE_VIGENCIA !== undefined) {
-
                             let arrDATA_DE_VIGENCIA = DATA_DE_VIGENCIA.split("/");
                             if (arrDATA_DE_VIGENCIA[2] >= 2019 || SITUACAO_DA_OBRA == "EM ANDAMENTO" || SITUACAO_DA_OBRA == "PARALISADA" || SITUACAO_DA_OBRA == "SOBRESTADO") {
                                 controle = 1;
+                                PROCESSO = MeusDados[i]['PROCESSO'];
+                                CONVENENTE = MeusDados[i]['CONVENENTE'];
+                                MODALIDADE = MeusDados[i]['MODALIDADE'];
+                                BAIRRO = MeusDados[i]['BAIRRO'];
+                                OBJETO = MeusDados[i]['OBJETO'];
+                                SITUACAO_DA_OBRA = MeusDados[i]['SITUACAO DA OBRA'];
+                                QUANT_PREVISTA = MeusDados[i]['QUANT. PREVISTA'];
+                                ENCAMINHAMENTO_PARA_SOLUCAO = MeusDados[i]['ENCAMINHAMENTO PARA SOLUÇÃO '];
+                                QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS = MeusDados[i]['QUANT. DE PARCELAS REALIZADAS / BENEFICIÁRIOS CADASTRADOS'];
+                                VALOR_TOTAL_DA_OBRA_PREVISTO_RS = MeusDados[i]['VALOR TOTAL DA OBRA - PREVISTO - R$'];
+                                RECURSO_FEDERAL_MUNICIPAL_OSC_PREVISTO_RS = MeusDados[i]['RECURSO FEDERAL/MUNICIPAL/OSC - PREVISTO - R$'];
+                                RECURSO_ESTADUAL_PREVISTO_RS = MeusDados[i]['RECURSO ESTADUAL - PREVISTO - R$'];
+                                RECURSO_ESTADUAL_ENTREGUE = MeusDados[i]['RECURSO ESTADUAL ENTREGUE - R$'];
+                                IRREGULARIDADE = MeusDados[i]['IRREGULARIDADE'];
+                                ACAO2 = MeusDados[i]['AÇÃO2'];
 
                                 let tamanho = SITUACAO_DA_OBRA.length;
                                 let tranforma = SITUACAO_DA_OBRA.toLocaleLowerCase().slice(1, tamanho);
                                 let aux = SITUACAO_DA_OBRA[0].toLocaleUpperCase() + tranforma;
+
                                 if (arrDATA_DE_VIGENCIA[2] < 2019) {
-                                    document.getElementById("relatorio").value += "\nAnterior à gestão atual";
+                                    document.getElementById("relatorio").value += "\n*Anterior à gestão atual - Ano de vigência: " + arrDATA_DE_VIGENCIA[2];
                                 }
+
                                 document.getElementById("relatorio").value += "\n" + aux + "\n";
                                 document.getElementById("relatorio").value += PROCESSO + "\n";
+
                                 if (MODALIDADE != "COMUNITÁRIO") {
 
-
                                     if (QUANT_PREVISTA > 1) {
-                                        document.getElementById("relatorio").value += BAIRRO + " - " + OBJETO + "s PREVISTAS\n";
+                                        document.getElementById("relatorio").value += BAIRRO + " - " + OBJETO + " PREVISTAS\n";
                                         if (SITUACAO_DA_OBRA == "CONCLUÍDA" || SITUACAO_DA_OBRA == "ENTREGUE") {
                                             document.getElementById("relatorio").value += "Concluído/Entregue: " + QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS + " UHs\n";
                                         }
@@ -123,8 +102,7 @@ function Processa(MeusDados) {
                                             document.getElementById("relatorio").value += "Concluído/Entregue: " + QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS + " UH\n";
                                         }
                                     }
-                                    //IF CONCLUIDO OU ENTREGUE? SIM: document.getElementById("relatorio").value += "Concluido/Entregue: " + QUANT_DE_PARCELAS_REALIZADAS_BENEFICIARIOS_CADASTRADOS +  "\n";
-
+                            
                                 }
                                 else {
                                     document.getElementById("relatorio").value += BAIRRO + " - " + OBJETO + "\n";
@@ -149,14 +127,14 @@ function Processa(MeusDados) {
                     }
                 }
             } if (controle == 0) {
-                document.getElementById("relatorio").value += "Não constam obras";
+                document.getElementById("relatorio").value += "Não constam obras\n";
             }
 
             let dbRef = firebase.database().ref();
             dbRef.child("/Relatorio").child(municipio_tela).child("AluguelSocial").get().then((snapshot) => {
                 if (snapshot.exists()) {
-                    var TextoAluguelSocial = snapshot.val();
-                    var arrAluguelSocial = TextoAluguelSocial.split("&");
+                    let TextoAluguelSocial = snapshot.val();
+                    let arrAluguelSocial = TextoAluguelSocial.split("&");
                     document.getElementById("relatorio").value += "\n\n_Aluguel Social_\n";
                     for (let i = 0; i < arrAluguelSocial.length - 1; i++) {
                         document.getElementById("relatorio").value += arrAluguelSocial[i] + "\n";
@@ -190,8 +168,7 @@ function Processa(MeusDados) {
 
             dbRef.child("/Relatorio").child(municipio_tela).child("DataRelatorio").get().then((snapshot) => {
                 if (snapshot.exists()) {
-                    debugger
-                    var TextoDataRelatorio = snapshot.val();
+                    let TextoDataRelatorio = snapshot.val();
                     document.getElementById("relatorio").value += "*" + municipio_tela + "* - " + TextoDataRelatorio + "\n";
                     document.getElementById("relatorio").value += "\n_Crédito Parceria_\n";
                 }
@@ -202,8 +179,8 @@ function Processa(MeusDados) {
 
             dbRef.child("/Relatorio").child(municipio_tela).child("CreditoParceria").get().then((snapshot) => {
                 if (snapshot.exists()) {
-                    var TextoCredito = snapshot.val();
-                    var arrTextoCredito = TextoCredito.split("&");
+                    let TextoCredito = snapshot.val();
+                    let arrTextoCredito = TextoCredito.split("&");
                     for (let i = 0; i < arrTextoCredito.length - 1; i++) {
                         document.getElementById("relatorio").value += arrTextoCredito[i] + "\n";
                     }
@@ -213,8 +190,8 @@ function Processa(MeusDados) {
 
             dbRef.child("/Relatorio").child(municipio_tela).child("AluguelSocial").get().then((snapshot) => {
                 if (snapshot.exists()) {
-                    var TextoAluguelSocial = snapshot.val();
-                    var arrAluguelSocial = TextoAluguelSocial.split("&");
+                    let TextoAluguelSocial = snapshot.val();
+                    let arrAluguelSocial = TextoAluguelSocial.split("&");
                     document.getElementById("relatorio").value += "_Aluguel Social_\n";
                     for (let i = 0; i < arrAluguelSocial.length - 1; i++) {
                         document.getElementById("relatorio").value += arrAluguelSocial[i] + "\n";
@@ -226,8 +203,8 @@ function Processa(MeusDados) {
 
             dbRef.child("/Relatorio").child(municipio_tela).child("GoiasSocial").get().then((snapshot) => {
                 if (snapshot.exists()) {
-                    var TextoGoiasSocial = snapshot.val();
-                    var arrTextoGoiasSocial = TextoGoiasSocial.split("&");
+                    let TextoGoiasSocial = snapshot.val();
+                    let arrTextoGoiasSocial = TextoGoiasSocial.split("&");
                     document.getElementById("relatorio").value += "_Goiás Social_\n";
                     for (let i = 0; i < arrTextoGoiasSocial.length - 1; i++) {
                         document.getElementById("relatorio").value += arrTextoGoiasSocial[i] + "\n";
@@ -244,17 +221,17 @@ function Processa(MeusDados) {
 
 function salvarBD() {
     if (document.getElementById("relatorio").value != "") {
-        var AluguelSocial = "";
-        var CreditoParceria = "";
-        var GoiasSocial = "";
-        var Municipio = document.getElementById("municipio").value;
-        const date = new Date();
-        var DataRelatorio = date.toLocaleDateString();
-        var texto = document.getElementById("relatorio").value;
-        var arrTexto = texto.split("\n");
-        var posicaoCreditoParceria = arrTexto.indexOf('_Crédito Parceria_');
-        var posicaoAluguelSocial = arrTexto.indexOf('_Aluguel Social_');
-        var posicaoGoiasSocial = arrTexto.indexOf('_Goiás Social_');
+        let AluguelSocial = "";
+        let CreditoParceria = "";
+        let GoiasSocial = "";
+        let Municipio = document.getElementById("municipio").value;
+        let date = new Date();
+        let DataRelatorio = date.toLocaleDateString();
+        let texto = document.getElementById("relatorio").value;
+        let arrTexto = texto.split("\n");
+        let posicaoCreditoParceria = arrTexto.indexOf('_Crédito Parceria_');
+        let posicaoAluguelSocial = arrTexto.indexOf('_Aluguel Social_');
+        let posicaoGoiasSocial = arrTexto.indexOf('_Goiás Social_');
         console.log(arrTexto);
         console.log(posicaoCreditoParceria);
         console.log(posicaoAluguelSocial);
